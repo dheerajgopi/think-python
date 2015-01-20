2a92d95d59923fe340fbcfa65d8f4bd1ede19e15
dheerajgopi <dheerajgopinath@gmail.com>
2015-01-20 22:52:42 +0530

    reverse pair done

---
+++ Chapter10/reverse_pair.py
@@ -0,0 +1,53 @@
+# Finding reverse pair of every word in the word list
+
+# Creating the sorted word list
+wordlist =sorted([x.strip() for x in open('/home/dheeraj/repos/think-python/words.txt')])
+
+# Function for binary search
+def binary(word):
+
+	# first = 1st index in the list..initially 0
+	first = 0
+
+	# last = last index in the list..initially len(list)
+	last = len(wordlist)
+
+	# loop for changing first and last index
+	while first < last:
+
+		middle = (first + last)/2
+		midval = wordlist[middle]
+
+		# first index changed to middle index if word is in the second half of list
+		if midval < word:
+
+			first = middle + 1
+
+		# last index changed to middle index if word is in the first half of list
+		elif midval > word:
+
+			last = middle
+
+		# if first index == last index return the index
+		else:
+
+			return middle
+
+	# if not in the list...
+	return -1
+
+# Creating list of reverse pairs 
+pairlist = [sorted((x,x[::-1])) for x in wordlist if binary(x[::-1]) != -1]
+
+# for storing final list
+revpairlist = []
+
+# For removing duplicates
+for x in pairlist:
+
+	if x not in revpairlist:
+
+		revpairlist.append(x)
+
+print revpairlist
+print len(revpairlist)
